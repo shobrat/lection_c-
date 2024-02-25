63a7e7d38d5bcfcee5de724505133302b8dc86c5
Sharasul <sh.haqnazarov@bipartners.uz>
2024-02-25 08:42:07 +0500

    add Task1

---
+++ Lesson2/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] arr = new int[5];
+
+arr[0] = 5;
+arr[1] = 6;
+arr[2] = 8;
+arr[3] = 9;
+arr[4] = 2;
+
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+int[] arr2 = new int[5](1, 2, 3, 4, 5);
+
+int[] arr3 = (1, 2, 3, 4, 5);
