78ea8602b5b10fb279fcc4cbd5253b9f4283aed5
Sharasul <sh.haqnazarov@bipartners.uz>
2024-02-25 09:13:20 +0500

    add Task3

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = 10;
+int[] array = {2, -3, 4, -5, 6, 7, 3, 9, 1, -2};
+int i = 0;
+
+while (i < n)
+{
+    if (array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+
+}
