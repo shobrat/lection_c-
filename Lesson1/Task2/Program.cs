1bff8566639d969297ef1f7683580c641736b00c
Шарасул Хакназаров <sh.haqnazarov@bipartners.uz>
2024-02-25 07:41:19 +0500

    add changes

--- Lesson1/Task2/Program.cs
+++ Lesson1/Task2/Program.cs
@@ -13,7 +13,14 @@ double sum3 = d1 + i1;
 
 int product1 = i1 * i2;
 long product2 = l1 * i1;
-double product3 = i1*d1;
+double product3 = i1 * d1;
 Console.WriteLine(product1);
 Console.WriteLine(product2);
 Console.WriteLine(product3);
+
+int quotien = i1 / i2;
+Console.WriteLine(quotien);
+
+d1 = 6;
+double quotient2 = i1 / d1;
+Console.WriteLine(quotient2);
--- Lesson1/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Lesson1/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39ff546cf724604e8da59ebd99ff1758ed8f9f99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e251ca7b43664c524b455d207719a272cf433396")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
