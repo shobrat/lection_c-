9074ebb6deeb98e5f44d490d83faa68dfcfacc82
Sharasul <sh.haqnazarov@bipartners.uz>
2024-02-25 11:49:29 +0500

    add Task3

---
+++ Lesson3/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿void ZeroEvenElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+int[] array = { 1, 2, 3, 4, 5, -6, -10 };
+ZeroEvenElements(array);
+PrintArray(array);
