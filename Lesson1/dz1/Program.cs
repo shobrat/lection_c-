fc29c5c9095f44c31185e2a1a143532ec172fc04
Шарасул Хакназаров <sh.haqnazarov@bipartners.uz>
2024-02-25 07:53:08 +0500

    add dz1

---
+++ Lesson1/dz1/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 3;
+int b = 4;
+int c = 5;
+int d = 6;
+
+double product = a * b;
+int sum = c + d;
+double quotion = product / sum;
+
+Console.WriteLine(product);
+Console.WriteLine(sum);
+Console.WriteLine(quotion);
