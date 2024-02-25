1dc2603bdf1dcdbe742cd1712be5e2fbe158f72c
Sharasul <sh.haqnazarov@bipartners.uz>
2024-02-25 10:26:28 +0500

    add Task4

--- Lesson2/Task4/obj/Debug/net8.0/Task4.AssemblyInfo.cs
+++ Lesson2/Task4/obj/Debug/net8.0/Task4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78ea8602b5b10fb279fcc4cbd5253b9f4283aed5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+243e17516346e8067d535a4732e31b7ffa9868d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
