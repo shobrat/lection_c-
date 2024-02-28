1b41c79488b76a6ee06ff959c22dcc0e1aeac3c0
Sharasul <sh.haqnazarov@bipartners.uz>
2024-02-28 20:30:01 +0500

    Prorgamm.cs

---
+++ Lesson1/Program.cs
@@ -0,0 +1,29 @@
+﻿int a = 6;
+int b = 7;
+int c = 9;
+int d = 4;
+int e = 10;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+
+if (c > max)
+{
+    max = b;
+}
+
+if (d > max)
+{
+    max = b;
+}
+
+if (e > max)
+{
+    max = b;
+}
+
+Console.WriteLine(max);
