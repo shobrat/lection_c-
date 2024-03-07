b49c7bcf42bf6a0847165f0291cff22439899d35
Xaknazarov Sharasul <741922@bk.ru>
2024-03-07 16:50:45 +0500

    add changes in folder Task3

---
+++ Lesson4/Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿
+string GetLettersFromString(string s)
+{
+    string letters = "";
+    foreach (char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+string str = Console.ReadLine();
+
+string result = GetLettersFromString(str);
+Console.WriteLine(result);
