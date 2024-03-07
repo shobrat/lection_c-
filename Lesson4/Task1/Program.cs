248c3abd984860b3848f656daf79cb47b9f88c82
Xaknazarov Sharasul <741922@bk.ru>
2024-03-07 12:49:10 +0500

    create new Folder Lesson4

---
+++ Lesson4/Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿
+int[,] CreateMatrix(int rowCount, int columsCount)
+{
+    int[,] matrix = new int[rowCount, columsCount];
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 4} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix=CreateMatrix(4,5);
+ShowMatrix(matrix);
